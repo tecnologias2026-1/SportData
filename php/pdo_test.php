b181b4329af097e3f9aaa65e4d78b450014d1f31
Jose Bernal <jmbernalmunoz@gmail.com>
2026-05-28 02:40:33 -0500

    Delete php directory

--- php/pdo_test.php
+++
@@ -1,8 +0,0 @@
-﻿<?php
-$dsn = "mysql:host=localhost:1900;dbname=sportdata;charset=utf8mb4";
-try {
-    $pdo = new PDO($dsn, 'root', 'Ad2556229', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-    echo 'DB OK';
-} catch (PDOException $e) {
-    echo 'ERROR: ' . $e->getMessage();
-}
